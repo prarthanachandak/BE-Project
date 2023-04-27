557d364ea1116443e58e0d2bb2369a8b149bb68b
Prarthana <69465478+prarthanachandak@users.noreply.github.com>
2023-04-27 21:53:17 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const http = require('http').createServer(app);
 const mongoose = require('mongoose');
 const socketio = require('socket.io')
 const io = socketio(http);
-const mongoDB = "mongodb+srv://prarthanachandak:UkF3LRfaFocpjKP9@cluster0.ye7gx6o.mongodb.net/?retryWrites=true&w=majority";
+const mongoDB = "yourcredentials";
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('connected')).catch(err => console.log(err))
 const { addUser, getUser, removeUser } = require('./helper');
 const Message = require('./models/Message');
